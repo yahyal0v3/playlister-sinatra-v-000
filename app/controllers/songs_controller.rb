a13fee5a1f8734a97f71cc7541780b46173b4cbf
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-01-28 19:06:42 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+require 'rack-flash'
+
+class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+
+    if @song.artist_id == nil
+      artist = Artist.create(params[:artist])
+      artist.songs << @song
+      artist.save
+    end
+
+    params[:genres].each do |genre|
+      @song.genres << Genre.find(genre)
+    end
+
+    flash[:message] = "Successfully created song."
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
+end
